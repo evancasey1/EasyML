590328c9b5d3bba5e61f474dd7020275a3aa1c9a
Evan Casey <ecasey313@gmail.com>
2018-09-18 17:17:57 -0400

    Add form to upload a CSV file

--- easyml/mainsite/urls.py
+++ easyml/mainsite/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('upload/', TemplateView.as_view(template_name='upload_csv.html'), name='upload'),
+    path('upload/csv/', views.upload_csv, name='upload_csv'),
 ]
--- easyml/mainsite/views.py
+++ easyml/mainsite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.urls import reverse_lazy
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from .forms import CustomUserCreationForm
 
@@ -12,3 +13,31 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def upload_csv(request):
+    data = {}
+    if "GET" == request.method:
+        return render(request, "upload_csv.html", data)
+
+    # if not GET, then proceed
+    try:
+        csv_file = request.FILES["csv_file"]
+        if not csv_file.name.endswith('.csv'):
+            messages.error(request, 'File is not CSV type')
+            return HttpResponseRedirect(reverse("upload_csv"))
+        # if file is too large, return
+        if csv_file.multiple_chunks():
+            messages.error(request, "Uploaded file is too big (%.2f MB)." % (csv_file.size / (1000 * 1000),))
+            return HttpResponseRedirect(reverse("upload_csv"))
+
+        file_data = csv_file.read().decode("utf-8")
+
+        lines = file_data.split("\n")
+        # loop over the lines and save them in db. If error , store as string and then display
+        for line in lines:
+            print(line)
+
+    except Exception as e:
+        messages.error(request, "Unable to upload file. " + repr(e))
+
+    return HttpResponseRedirect("/")
