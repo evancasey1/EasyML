2895fa6b99f4a950b8a04976b9ff0481e5c47f54
Evan Casey <ecasey313@gmail.com>
2018-10-04 15:26:14 -0400

    LDA and decision tree with smart depth selection

--- easyml/helpers/model_builder.py
+++ easyml/helpers/model_builder.py
@@ -13,8 +13,11 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeRegressor
 
-from .constants import COLUMN_TYPE, ALGORITHM
+from .constants import COLUMN_TYPE, ALGORITHM, algorithm_name_map
 from mainsite.models import CsvFile, CsvFileData, MLModel
 from .util import get_dataframe
 
@@ -31,26 +34,37 @@ def create_model(algorithm_type, file_id):
     target_data = file_data.filter(type=COLUMN_TYPE.TARGET)
 
     model = None
-    name = None
+    name = algorithm_name_map[str(algorithm_type)]
 
     input_df = get_dataframe(input_data)
     target_df = get_dataframe(target_data)
 
     if algorithm_type == ALGORITHM.LINEAR_REGRESSION:
-        name = "Linear Regression"
         model = create_linear_regression_model(input_df, target_df)
 
     elif algorithm_type == ALGORITHM.K_NEAREST_NEIGHBORS:
-        name = "K Nearest Neighbors"
         model = create_k_nearest_neightbors_model(input_df, target_df)
 
     elif algorithm_type == ALGORITHM.LOGISTIC_REGRESSION:
-        name = "Logistic Regression"
         model = create_logistic_regression_model(input_df, target_df)
 
     elif algorithm_type == ALGORITHM.NEAREST_CENTROID:
-        name = "Nearest Centroid"
         model = create_nearest_centroid(input_df, target_df)
+
+    elif algorithm_type == ALGORITHM.LINEAR_DISCRIMINANT_ANALYSIS:
+        model = create_linear_discriminant_analysis(input_df, target_df)
+
+    elif algorithm_type == ALGORITHM.DECISION_TREE:
+        model = create_decision_tree(input_df, target_df)
+
+    elif algorithm_type == ALGORITHM.NAIVE_BAYES:
+        pass
+
+    elif algorithm_type == ALGORITHM.RANDOM_FOREST:
+        pass
+
+    elif algorithm_type == ALGORITHM.SUPPORT_VECTOR_MACHINES:
+        pass
 
     if model:
         save_model(model, name, file_id)
@@ -85,6 +99,32 @@ def create_logistic_regression_model(input_df, target_df):
 
     clf = gs.fit(input_df, target_df)
     return clf
+
+def create_linear_discriminant_analysis(input_df, target_df):
+    clf = LinearDiscriminantAnalysis()
+    clf.fit(input_df, target_df)
+
+    return clf
+
+def create_decision_tree(input_df, target_df):
+    x_train, x_valid, y_train, y_valid = train_test_split(input_df, target_df, test_size=0.20)
+    r2_lst = []
+    depth_start = 1
+    depth_max = 10
+
+    # Select model with best r^2 and least depth
+    for i in range(depth_start, depth_max + 1):
+        dt_regr = DecisionTreeRegressor(max_depth=i)
+        dt_regr.fit(x_train, y_train)
+        r2_lst.append(dt_regr.score(x_valid, y_valid))
+
+    max_depth, r2 = min(enumerate(r2_lst), key=lambda x: abs(x[1] - 1))
+
+    print("Depth:", max_depth)
+    print("r2:", r2)
+
+    dt_regr_final = DecisionTreeRegressor(max_depth=max_depth).fit(input_df, target_df)
+    return dt_regr_final
 
 def create_k_nearest_neightbors_model(input_df, target_df):
     neighbors = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(input_df)
