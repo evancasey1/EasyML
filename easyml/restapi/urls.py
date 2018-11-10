d1212eeafcd16861046891a14aae2af43d67b48c
Evan Casey <ecasey313@gmail.com>
2018-11-10 18:48:23 -0500

    Use API to get accuracy

--- easyml/restapi/urls.py
+++ easyml/restapi/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get_file_headers/<int:file_id>', views.GetFileHeaders.as_view(), name='get_file_headers'),
+    path('get_accuracy/<int:ffid>&<int:sfid>&<str:header>&<str:method>', views.GetAccuracy.as_view(), name='get_accuracy'),
 ]
--- easyml/restapi/views.py
+++ easyml/restapi/views.py
@@ -9,6 +9,8 @@ from rest_framework.exceptions import (
 from rest_framework import viewsets, mixins
 from django.http import JsonResponse, Http404
 from django.http import HttpResponseForbidden
+
+from helpers.util import *
 from mainsite.models import *
 
 class BaseUserView(APIView):
@@ -35,3 +37,64 @@ class GetFileHeaders(BaseUserView):
         header_data = {'headers': sorted(list(data_raw))}
 
         return JsonResponse(header_data)
+
+class GetAccuracy(BaseUserView):
+
+    def get(self, request, ffid=None, sfid=None, header=None, method=None):
+        if not ffid:
+            raise APIException("First file_id not provided")
+
+        if not sfid:
+            raise APIException("Second file_id not provided")
+
+        if not header:
+            raise APIException("Header not provided")
+
+        if not method:
+            raise APIException("Method not provided")
+
+        ffid = int(ffid)
+        sfid = int(sfid)
+
+        first_data = CsvFileData.objects.filter(parent_file_id=ffid,
+                                                column_header=header) \
+            .order_by('row_num') \
+            .values_list('data', flat=True)
+
+        second_data = CsvFileData.objects.filter(parent_file_id=sfid,
+                                                 column_header=header) \
+            .order_by('row_num') \
+            .values_list('data', flat=True)
+
+        if len(first_data) != len(second_data):
+            messages = []
+            if len(second_data) == 0:
+                fname = CsvFile.objects.get(id=sfid).display_name
+                messages.append('Header "{}" does not exist in file "{}"'.format(header, fname))
+
+            messages.append('Length of the two files is not identical. {} vs {} rows.'
+                            .format(len(first_data), len(second_data)))
+
+            return JsonResponse({
+                'status_code': 500,
+                'messages': messages
+            })
+
+        if 'accuracy' in method.lower():
+            acc = str(get_match_acc(first_data, second_data)) + "%"
+            accuracy_type = 'Accuracy'
+            method_sm = 'Accuracy'
+        else:
+            acc = get_r2(first_data, second_data)
+            accuracy_type = 'R^2'
+            method_sm = 'Correlation'
+
+        context = {
+            'status_code': 200,
+            'accuracy': acc,
+            'accuracy_type': accuracy_type,
+            'method_sm': method_sm,
+            'num_rows': len(first_data),
+        }
+
+        return JsonResponse(context)
